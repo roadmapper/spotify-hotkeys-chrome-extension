a4c23d5892842df92a688827bc5bdf9f3c591f59
Vinay Dandekar <vindansam@hotmail.com>
2018-05-22 16:21:56 -0400

    Change to use SoundCloud CSS classes

--- src/background.js
+++ src/background.js
@@ -1,25 +1,24 @@
 function onCommand(command) {
-  chrome.tabs.query({url: 'https://*.spotify.com/*'}, function(tabs) {
+  chrome.tabs.query({url: 'https://*.soundcloud.com/*'}, function(tabs) {
 
     // Open a spotify tab if one does not exist yet.
     if (tabs.length === 0) {
-      chrome.tabs.create({url: 'https://open.spotify.com'});
+      chrome.tabs.create({url: 'https://soundcloud.com'});
     }
 
     // Apply command on all spotify tabs.
     for (var tab of tabs) {
 
       var code = '';
-      if (tab.url.startsWith('https://play.spotify.com')) {
+      if (tab.url.startsWith('https://soundcloud.com')) {
         code = "document.getElementById('app-player').contentDocument.getElementById('" + command + "').click()";
       } else if (tab.url.startsWith('https://open.spotify.com')) {
         switch (command) {
-          case 'next': code = 'document.querySelector(".spoticon-skip-forward-16").click()'; break;
-          case 'previous': code = 'document.querySelector(".spoticon-skip-back-16").click()'; break;
-          case 'shuffle': code = 'document.querySelector(".spoticon-shuffle-16").click()'; break;
-          case 'repeat': code = 'document.querySelector(".spoticon-repeat-16").click()'; break;
-          case 'track-add': code = '(document.querySelector(".spoticon-add-16") || document.querySelector(".spoticon-added-16")).click()'; break;
-          case 'play-pause': code = '(document.querySelector(".spoticon-play-16") || document.querySelector(".spoticon-pause-16")).click()'; break;
+          case 'next': code = 'document.querySelector(".playControls__next").click()'; break;
+          case 'previous': code = 'document.querySelector(".playControls__prev").click()'; break;
+          case 'shuffle': code = 'document.querySelector(".shuffleControl").click()'; break;
+          case 'repeat': code = 'document.querySelector(".playControls__repeat").click()'; break;
+          case 'play-pause': code = 'document.querySelector(".playControls__play")'; break;
         }
       }
       if (code.length) {
